4fab65c4fa98c55aa437cb5bf420973483478001
Ludovic Sitalapresad <ludovic.sitalapresad@gmail.com>
2014-03-21 12:01:25 +0400

    Event

---
+++ server.js
@@ -0,0 +1,8 @@
+var EventEmitter = require('events').EventEmitter;
+var jeu = new EventEmitter();
+
+jeu.on('gameover', function(message){
+	console.log(message);
+});
+
+jeu.emit('gameover', 'Vous avez perdu !');
